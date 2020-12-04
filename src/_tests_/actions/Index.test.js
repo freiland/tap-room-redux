d3da0d83303c702ab64c8a5180ec7f4ca90f6628
Forrest Reiland <you@example.com>
2020-12-04 09:23:00 -0800

    add test for ADD_TICKET action

--- src/_tests_/actions/Index.test.js
+++ src/_tests_/actions/Index.test.js
@@ -2,13 +2,28 @@ import { Item } from 'react-bootstrap/lib/Breadcrumb';
 import * as actions from '../../actions/Index.js';
 
 describe('tap room actions', () => {
-  Item('deleteKeg should create DELETE_KEG action', () => {
+  it('deleteKeg should create DELETE_KEG action', () => {
     expect(actions.deleteKeg(1).toEqual({
       type: 'DELETE_KEG',
       id:1
     }))
   });
 
+  it('toggleForm should create TOGGLE_FORM action', () => {
+    expect(actions.toggleForm()).toEqual({
+      type: 'TOGGLE_FORM'
+    });
+  });
 
-
+  it('addKeg should create ADD_TICKER action', () => {
+    expect.addTicket({name: 'Winter Ale', price: 5, brand: 'test', pintsAvail: 125, alcoholContent: 3, id: 1}).toEqual({
+      type: 'ADD_KEG',
+      name: 'Winter Ale',
+      price: 5,
+      brand: 'test',
+      pintsAvail: 125,
+      alcoholContent: 3,
+      id: 1
+    });
+  });
 });
